f01de6ff5a0a5c8f739d39ffdefe27a9565afcd4
Miguel Andres MF <miguelandresmarinf@gmail.com>
2025-08-03 02:22:30 -0500

    Inicio del proyecto con Spring Initializr

---
+++ src/main/java/com/alura/looptalk/LooptalkApplication.java
@@ -0,0 +1,13 @@
+package com.alura.looptalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooptalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LooptalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/looptalk/LooptalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.looptalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooptalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
