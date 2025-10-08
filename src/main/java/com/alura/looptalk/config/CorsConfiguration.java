b81df5016ed95bde1eeabbc8ff5e686982fdb865
Miguel Andres MF <miguelandresmarinf@gmail.com>
2025-10-07 20:09:49 -0500

    add: cors config

---
+++ src/main/java/com/alura/looptalk/config/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.alura.looptalk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000",           // Desarrollo local
+                        "http://localhost:3001"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
